588204a16646d06cbf19554c8b6bd5f9aed8cc7e
MUHAMMAD ZIKRI BIN KASHIM <zikrimzk@gmail.com>
2025-11-30 18:13:41 +0800

    Backend : Part 1C

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Hash;
+use Exception;
 
 class AuthController extends Controller
 {
