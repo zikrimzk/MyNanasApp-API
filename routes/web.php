6961205dbbc67f7656ca45d5db95aaddd682e3b9
MUHAMMAD ZIKRI BIN KASHIM <zikrimzk@gmail.com>
2025-12-04 09:47:05 +0800

    SERVER : TEST 1A

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+/* [SERVER USE] */
+Route::get('/artisan/{cmd}/{key}', function ($cmd, $key) {
+    $secret = 'zikri123';
+
+    if ($key !== $secret) {
+        abort(403, 'Unauthorized');
+    }
+
+    $allowed = [
+        'migrate' => 'migrate',
+        'migratefresh' => 'migrate:fresh',
+        'key'     => 'key:generate',
+        'cache'   => 'config:cache',
+        'route'   => 'route:cache',
+        'view'    => 'view:cache',
+        'storage' => 'storage:link',
+        'optimize'   => 'optimize:clear',
+        'composer' => 'composer dump-autoload',
+    ];
+
+    if (!array_key_exists($cmd, $allowed)) {
+        return "Command not allowed.";
+    }
+
+    Artisan::call($allowed[$cmd]);
+    return nl2br(Artisan::output());
+});
 
 Route::get('/', function () {
     return view('welcome');
